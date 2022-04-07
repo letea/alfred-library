bbc2bc6ff4d4e8486e74cb8527a2a99c0386f3e2
Tea Lee <tealee@catchplay.com>
2022-04-07 10:15:32 +0800

    feat: outputList 加入關鍵字過濾的功能

--- src/outputList/index.js
+++ src/outputList/index.js
@@ -1,10 +1,34 @@
+// node modules
+import { argv } from "process";
+
+// local modules
 import { output } from "../output/index.js";
 
-const outputList = ({ items = {} } = {}) => {
+const outputList = ({ items = {}, matchKeys = [] } = {}) => {
+  const inputMatch = ({ items = [], matchKeys = [] } = {}) => {
+    const keywords = argv?.slice(2) || [];
+
+    if (keywords.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      for (let key of matchKeys) {
+        const value = item?.[key] || "";
+        for (let keyword of keywords) {
+          if (value.toLowerCase().indexOf(keyword.toLowerCase()) >= 0) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
+  };
+
   output(
     JSON.stringify(
       {
-        items
+        items: inputMatch({ items, matchKeys })
       },
       null,
       "\t"
